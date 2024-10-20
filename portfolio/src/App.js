b917823bec8a31516e613150c9ed64f073eb1588
angular_standard <sonishiv309@gmail.com>
2024-10-20 17:20:55 +0530

    created navbar component with logo

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,34 @@
+import {BrowserRouter} from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Feedbacks from './components/Feedbacks';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Loader from './components/Loader';
+import Works from './components/Works';
+import Tech from './components/Tech';
+import { StarsCanvas } from './components';
+
 const  App=()=> {
   return (
-    <div>
-      <h1 className='text-5xl font-bold'>Software Developer Portfolio</h1>
+    <BrowserRouter>
+    <div className='relative bg-primary z-0'>
+     <div className='bg-hero-pattern bg-cover bg-repeat bg-center'>
+     <Navbar />
+     <Hero />
+     </div>
+     <About />
+     <Experience />
+     <Tech />
+     <Works />
+     <Feedbacks />
+     <div className='relative z-0'>
+     <Contact />
+     <StarsCanvas />
+     </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useState,useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import {styles} from '../style.js';
+import {navLinks} from '../contstants';
+import { logo,menu,close} from '../assets/index.js';
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed z-20 top-0 bg-primary `}>
+    <div className='w-full flex items-center justify-between max-w-7xl max-autos'>
+     <Link to='/' className='w-full flex items-center gap-2' onClick={()=>{
+      setActive("");
+      window.scrollTo(0,0);
+     }}>
+      <img src={logo} alt='logo' className='w-9 h-9 object-contain ' />
+      <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Shiv Soni &nbsp;
+            <span className='sm:block hidden'> | Full-stack Software Engineer</span>
+          </p>
+     </Link>
+     <ul className='list-none hidden sm:flex flex-row gap-10'>
+    {
+      navLinks.map((items)=>(
+        <li
+        key={items.id}
+        className={`${active===items.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
+        onClick={()=>setActive(items.title)}
+        >
+          {items.title}
+        </li>
+      ))
+    }
+     </ul>
+    </div>
+    </nav>
   )
 }
 
