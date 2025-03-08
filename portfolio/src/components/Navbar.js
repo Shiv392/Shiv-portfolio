091e79de522f1f782acc956114d7d5685b5eec06
angular_standard <sonishiv309@gmail.com>
2025-03-08 22:32:44 +0530

    added border bottom on hover effect in navbar link

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -37,78 +37,77 @@ const Navbar = () => {
 
   return (
     <>
-    <nav
-      className={`${
-        styles.paddingX
-      } w-full flex items-center py-5 fixed top-0 z-20 ${
-        scrolled ? "bg-primary" : "bg-transparent"
-      }`}
-    >
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link
-          to='/'
-          className='flex items-center gap-2'
-          onClick={() => {
-            setActive("");
-            window.scrollTo(0, 0);
-          }}
-        >
-          {/* <img src={logo} alt='logo' className='w-9 h-9 object-contain' /> */}
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Shiv Soni &nbsp;
-            <span className='sm:block hidden'> | Software Development Engineer</span>
-          </p>
-        </Link>
-
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {navLinks.map((nav) => (
-            <li
-              key={nav.id}
-              className={`${
-                active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(nav.title)}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
-        </ul>
-
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
-          {
-            toggle ? <IconX  className="cursor-pointer" onClick={()=> setToggle(!toggle)} size={25} /> :
-             <IconMenu2 className="cursor-pointer" onClick={()=> setToggle(!toggle)} size={25} />
-          }
-          
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+      <nav
+        className={`${styles.paddingX
+          } w-full flex items-center py-5 fixed top-0 z-20 ${scrolled ? "bg-primary" : "bg-transparent"
+          }`}
+      >
+        <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+          <Link
+            to='/'
+            className='flex items-center gap-2'
+            onClick={() => {
+              setActive("");
+              window.scrollTo(0, 0);
+            }}
           >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
-              {navLinks.map((nav) => (
-                <li
-                  key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-secondary"
-                  }`}
-                  onClick={() => {
-                    setToggle(!toggle);
-                    setActive(nav.title);
-                  }}
-                >
-                  <a href={`#${nav.id}`}>{nav.title}</a>
-                </li>
-              ))}
-            </ul>
+            {/* <img src={logo} alt='logo' className='w-9 h-9 object-contain' /> */}
+            <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+              Shiv Soni &nbsp;
+              <span className='sm:block hidden'> | Software Development Engineer</span>
+            </p>
+          </Link>
+
+          <ul className='list-none hidden sm:flex flex-row gap-10'>
+            {navLinks.map((nav) => (
+              <li
+                key={nav.id}
+                className={`relative ${active === nav.title ? "text-white" : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer 
+                  before:absolute before:bottom-[-5px] before:left-0 before:w-full 
+                  before:h-[2px] before:bg-white before:scale-x-0 
+                  before:origin-left before:transition-transform before:duration-300 
+                  hover:before:scale-x-100`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+
+          <div className='sm:hidden flex flex-1 justify-end items-center'>
+            {
+              toggle ? <IconX className="cursor-pointer" onClick={() => setToggle(!toggle)} size={25} /> :
+                <IconMenu2 className="cursor-pointer" onClick={() => setToggle(!toggle)} size={25} />
+            }
+
+            <div
+              className={`${!toggle ? "hidden" : "flex"
+                } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            >
+              <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+                {navLinks.map((nav) => (
+                  <li
+                    key={nav.id}
+                    className={`font-poppins font-medium cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-secondary"
+                      }`}
+                    onClick={() => {
+                      setToggle(!toggle);
+                      setActive(nav.title);
+                    }}
+                  >
+                    <a href={`#${nav.id}`}>{nav.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-         <motion.div
-         className='fixed top-0 left-0 right-0 h-1 bg-white origin-left z-50'
-         style={{ scaleX }} // Connect the progress bar to scroll progress
-       />
+      </nav>
+      <motion.div
+        className='fixed top-0 left-0 right-0 h-1 bg-white origin-left z-50'
+        style={{ scaleX }} // Connect the progress bar to scroll progress
+      />
     </>
   );
 };
