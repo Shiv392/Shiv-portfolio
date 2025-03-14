2cde2c641cdb99ec18a894c082092253ff807fb2
angular_standard <sonishiv309@gmail.com>
2025-03-15 03:18:47 +0530

    improved loader by adding loading text.

--- portfolio/src/components/Loader.js
+++ portfolio/src/components/Loader.js
@@ -13,14 +13,15 @@ const Loader = () => {
         <p className="mt-2 text-lg text-blue-500 font-semibold">Loading...</p>
       </div> */}
 
-     <div class="pyramid-loader">
-     <div class="wrapper">
-      <span class="side side1"></span>
-      <span class="side side2"></span>
-      <span class="side side3"></span>
-      <span class="side side4"></span>
-      <span class="shadow"></span>
+     <div className="pyramid-loader">
+     <div className="wrapper">
+      <span className="side side1"></span>
+      <span className="side side2"></span>
+      <span className="side side3"></span>
+      <span className="side side4"></span>
+      <span className="shadow"></span>
      </div>  
+     <p className="text-lg text-blue-500 font-semibold text-center">Loading...</p>
     </div>
     </Html>
   );
